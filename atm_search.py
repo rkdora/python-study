fa07e018a186c3053add650e8a297f1577b16895
ryuto <rryuto5210@gmail.com>
2019-05-12 15:18:10 +0900

    Get table shops

--- atm_search.py
+++ atm_search.py
@@ -1,21 +1,25 @@
-# coding: UTF-8
-# request_test.py
-
+import csv
 import requests
 from bs4 import BeautifulSoup
 
-# 取得したいURL
 url = "http://www.fukuokabank.co.jp/atmsearch/?area=%E5%8C%97%E4%B9%9D%E5%B7%9E%E5%B8%82%EF%BC%88%E8%A5%BF%E9%83%A8%EF%BC%89"
 
-# urlを引数に指定して、HTTPリクエストを送信してHTMLを取得
 response = requests.get(url)
 
-# 文字コードを自動でエンコーディング
 response.encoding = response.apparent_encoding
 
-# HTML解析
-bs = BeautifulSoup(response.text, 'html.parser')
-title_tag = bs.find('title')
+bs = BeautifulSoup(response.content, 'html.parser')
 
-# 取得したHTMLを表示
-print(title_tag.text)
+table = bs.findAll("div", {"class":"table_style02_block"})[0]
+
+rows = table.findAll("tr")
+
+csv_list = []
+
+with open("shops.csv", "w", encoding='utf-8') as file:
+    writer = csv.writer(file)
+    for row in rows:
+        csvRow = []
+        for cell in row.findAll(['td', 'th']):
+            csvRow.append(cell.get_text())
+        writer.writerow(csvRow)
